aaf90df720e6a3c6a1610263516b7cb909aa5e62
kailanajt <kailana.thompson12@gmail.com>
2022-08-09 12:59:50 -0500

    connecting site through database

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Item
 
-class Item: 
-  def __init__(self, name, sort, brand, store):
-    self.name = name
-    self.sort = sort
-    self.brand = brand
-    self.store = store
 
-items = [
-  Item('Bread', 'Gluten', 'Honey Wheat', 'Walmart'),
-  Item('Eggs', 'Animal Product', 'Great Value', 'Sams Club'),
-  Item('Milk', 'Dairy', 'Oaks Farm', 'Walmart'),
-  Item('Dog Food', 'Pets', 'Purina Puppy', 'Sams Club')
-]
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Grocery List</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def items_index(request):
-  return render(request, 'items/index.html', { 'items': items})
+  items = Item.objects.all()
+  return render(request, 'items/index.html', {'items': items})
