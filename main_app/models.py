748bd33e4614c4e3662836a05dbbf7c55b82882d
kailanajt <kailana.thompson12@gmail.com>
2022-08-10 13:07:21 -0500

    complete app aside css

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
@@ -7,6 +8,7 @@ class Item(models.Model):
   sort = models.CharField(max_length=100)
   brand = models.CharField(max_length=100)
   store = models.CharField(max_length=100)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Item
 
 
 
 # Create your views here.
-
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def items_index(request):
-  items = Item.objects.all()
+  items = Item.objects.filter(user=request.user)
   return render(request, 'items/index.html', {'items': items})
+
 
 def items_detail(request, item_id):
   item = Item.objects.get(id=item_id)
   return render(request, 'items/detail.html', {'item': item})
 
-class ItemCreate(CreateView):
+class ItemCreate(LoginRequiredMixin, CreateView):
   model = Item
-  fields = '__all__'
-  success_url = '/items/'
+  fields = ['name', 'sort', 'brand', 'store']
+  
 
-class ItemUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+
+class ItemUpdate(LoginRequiredMixin, UpdateView):
   model = Item
   fields = [ 'sort', 'brand', 'store']
 
-class ItemDelete(DeleteView):
+
+class ItemDelete(LoginRequiredMixin, DeleteView):
   model = Item
   success_url = '/items/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('items_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
